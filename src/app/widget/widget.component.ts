809e0c78ad4d3e6ada44ad9527c81fab31d3e894
Kohki Sato <you@example.com>
2019-02-16 17:44:52 +0900

    First commit

---
+++ src/app/widget/widget.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['./widget.component.css']
+})
+export class WidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+    console.log('onClick was called');
+  }
+
+}
